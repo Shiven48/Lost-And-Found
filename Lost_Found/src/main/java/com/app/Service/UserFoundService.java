4b9d0f7fdf1dfce2159016f133f8d07e189d43c4
Shiven48 <shevenroyal@gmail.com>
2024-11-16 07:09:25 +0530

    Added Credentials Repository

--- Lost_Found/src/main/java/com/app/Service/UserFoundService.java
+++ Lost_Found/src/main/java/com/app/Service/UserFoundService.java
@@ -1,22 +1,13 @@
 package com.app.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
-import com.app.Entity.User;
 import com.app.Entity.UserFound;
-//import com.app.Entity.UserLost;
-//
-//import jakarta.validation.constraints.Email;
-//import jakarta.validation.constraints.NotBlank;
-//import jakarta.validation.constraints.Size;
 import com.app.Repository.UserFoundRepository;
-
-//(48,"Shiven","Shiven@gmail.com","Shiven@123",LocalDateTime.now(),
-//		LocalDateTime.now(),"Virar", LocalTime.MIDNIGHT,tags)
 
 @Service
 public class UserFoundService {
@@ -28,19 +19,35 @@ public class UserFoundService {
 	}
 	
 	public UserFound getById(Long id) {
-		return userFoundRepository.findById(id).orElse(new UserFound());
+		try {
+			return userFoundRepository
+									.findById(id)
+									.orElse(new UserFound());
+		} catch(Exception e) {
+			 throw new RuntimeException("Failed to retrieve user with id: "+id, e);
+		}
 	}
 	
 	public UserFound postUserFound(UserFound user_Found) {
-		return null;
+		try {
+			UserFound user = new UserFound();
+			
+			userFoundRepository.save(user_Found);
+			// Check tags
+			// Add user
+			return null;
+		} catch(Exception e) {
+			throw new RuntimeException("Failed to post user: ", e);
+		}
 	}
 
 	public List<UserFound> getuserFoundAll() {
-		List<UserFound> AllUserFound = userFoundRepository.findAll();
-		if(AllUserFound != null) {
-			return AllUserFound;
+		try {
+			List<UserFound> AllUserFound = userFoundRepository.findAll();
+			return AllUserFound.isEmpty() ? Collections.emptyList() : AllUserFound;	
+		} catch(Exception e) {
+            throw new RuntimeException("Failed to retrieve users", e);
 		}
-		return new ArrayList<UserFound>();
 	}
 
 }
