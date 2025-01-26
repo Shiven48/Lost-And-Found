a0c123bfe6cd63e247a264e3196883268ab89308
Shiven48 <shevenroyal@gmail.com>
2025-01-26 21:08:39 +0530

    Added credentials request dto for the credentials

---
+++ Backend/src/main/java/com/app/Models/DTO/Credentials/CredentialsRequestDto.java
@@ -0,0 +1,17 @@
+package com.app.Models.DTO.Credentials;
+
+import jakarta.validation.constraints.*;
+
+public record CredentialsRequestDto(
+        @Email(message = "Please provide a valid email address",
+        regexp = "^(?=(.*[A-Z]))(?=(.*\\d))(?=(.*[\\W_]))[A-Za-z\\d\\W_]{8,25}@gmail\\.com$\n")
+        @NotBlank(message = "Email is required")
+        String email,
+
+        @NotBlank(message = "Password is required")
+        @Size(min = 8, max = 64, message = "Password must be between 8 and 64 characters")
+        String password,
+
+        @Pattern(regexp = "^ROLE_[A-Z]+$",message = "Role must start with 'ROLE_' followed by uppercase letters.")
+        String roles
+) { }
