c52a5528551e4677899a9dd7682cd7c5a3db2530
Shiven48 <shevenroyal@gmail.com>
2025-01-30 02:46:10 +0530

    Modified admin and credentials repo

--- Backend/src/main/java/com/app/Repository/CredentialsRepository.java
+++ Backend/src/main/java/com/app/Repository/CredentialsRepository.java
@@ -1,9 +1,14 @@
 package com.app.Repository;
 
+import com.app.Models.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.app.Entity.Credentials;
+import com.app.Models.Entities.Credentials;
+//import org.springframework.security.core.userdetails.User;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface CredentialsRepository extends JpaRepository<Credentials, Long> {
-
+    Optional<User> findByName(String name);
 }
