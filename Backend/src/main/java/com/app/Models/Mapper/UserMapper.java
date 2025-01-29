c9e6d47b9d626106ddf12f0bcf56ce8e4470b362
Shiven48 <shevenroyal@gmail.com>
2025-01-30 02:49:27 +0530

    Modified User Mapper class #1

--- Backend/src/main/java/com/app/Models/Mapper/UserMapper.java
+++ Backend/src/main/java/com/app/Models/Mapper/UserMapper.java
@@ -1,10 +1,13 @@
-package com.app.Entity.Mapper;
+package com.app.Models.Mapper;
 
-import com.app.Entity.DTO.User.*;
-import com.app.Entity.Models.Credentials;
-import com.app.Entity.Models.User;
+import com.app.Models.DTO.User.*;
+import com.app.Models.Entities.Credentials;
+import com.app.Models.Entities.User;
+import com.app.Models.Enums.Lost_Found;
+import com.app.Models.Interface.UserType;
 import com.app.Repository.CredentialsRepository;
 import com.app.Service.CredentialService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -20,70 +23,60 @@ public class UserMapper {
         this.credentialsRepository = credentialsRepository;
     }
 
-    public User userRequestDtoToUser(User user, UserRequestDto userRequestDto, Credentials credential) {
-//        user.setName(userRequestDto.name());
-//        user.setLoggedIn(userRequestDto.isLoggedIn());
-//
-//
-//
-//        user.setCredentials(credential);
-//        credential.setUser(user);
+//  Saved Credentials : Credentials{name='KetoAdm', email='shevenroyal@gmail.com', password='Some.password@me', roles='ROLE_ADMIN', registrationDate=null, lastModified=null, id=1}
+//  User{isLoggedIn=null, lost_found=null, credentials=null, itemsFound=[], itemsLost=[], registrationDate=null, lastModified=null, id=1}
 
-        return new User();
+    public UserResponseDto userToUserResponseDto(UserType user, Credentials credential) {
+        return new UserResponseDto(
+                user.getId(),
+                credential.getName(),
+                user.getLoggedIn()
+        );
     }
 
-    public User userRequestDtoToUser(User user, UserRequestDto userRequestDto) {
-        user.setName(userRequestDto.name());
+    public User userRequestDtoToUser(User user, Credentials credential, UserRequestDto userRequestDto) {
         user.setLoggedIn(userRequestDto.isLoggedIn());
-
+        user.setLost_found(Lost_Found.LOST);
+        user.setCredentials(credential);
         return user;
     }
 
-    public UserRequestDto UserToUserRequestDto(User user) {
-        return new UserRequestDto(
-                user.getName(),
-                user.getLoggedIn(),
-//                user.getCredentials().getId()
-                12083L
+    // Temporary
+    public User userRequestDtoToUser(User user, UserRequestDto requestDto) {
+        return  null;
+    }
+
+    public Credentials toUserResponseAdmin(@Valid UserRequestDto userRequestDto, String role) {
+        return new Credentials(
+                userRequestDto.email(),
+                userRequestDto.password(),
+                userRequestDto.name(),
+                role
         );
+    }
+
+    public Credentials toUserResponseUser(@Valid UserRequestDto userRequestDto, String role) {
+        return new Credentials(
+                userRequestDto.email(),
+                userRequestDto.password(),
+                userRequestDto.name(),
+                role
+        );
+    }
+
+    public UserRequestDto UserToUserRequestDto(User user) {
+       return null;
     }
 
     public UserDto toUserDto(User user){
-        return new UserDto(
-//                user.getUserId(),
-                user.getName(),
-//                user.getCredentials().getEmail()
-                "some@gmail.com"
-        );
-    }
-
-    public UserResponseDto userToUserResponseDto(User user) {
-        return new UserResponseDto(
-//                user.getUserId(),
-                user.getName(),
-                user.getLoggedIn(),
-//                user.getRegistrationDate(),
-//                user.getLastModified(),
-//                CredentialMapper.ToCredentialResponseDto(user.getCredentials())
-                CredentialMapper.ToCredentialResponseDto(new User())
-        );
+        return null;
     }
 
     public UserLostItemsDto toUserLostItemsDto(User user){
-        return new UserLostItemsDto(
-//                user.getUserId(),
-                user.getName(),
-                user.getLoggedIn(),
-                user.getItemsLost()
-        );
+        return null;
     }
 
     public UserFoundItemsDto toUserFoundItemsDto(User user) {
-        return new UserFoundItemsDto(
-//                user.getUserId(),
-                user.getName(),
-                user.getLoggedIn(),
-                user.getItemsFound()
-        );
+        return null;
     }
 }
